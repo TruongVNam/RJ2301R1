2f4f3a4e96f92ea7a2c7a6d03a2465d3bf00dcac
TruongVNam <truongnam.css.yds@gmail.com>
2023-02-03 20:36:52 +0700

    RJ2301R1
    Learning Reactjs

---
+++ Async-Awit-ES6/ES6/Module/app.js
@@ -0,0 +1,5 @@
+import { greet, LESSION, greeting } from './main.js'
+
+alert(greet);
+alert(LESSION);
+alert(greeting("Lan Phuong", "Hoc cach dung Module trong ES6"));
---
+++ Async-Awit-ES6/ES6/Module/main.js
@@ -0,0 +1,8 @@
+let greet = "Xin chao CODGYM ONLINE!";
+const LESSION = "Su dung Module trong ES6";
+
+function greeting(name, content) {
+    return name + " " + content;
+};
+
+export {greet, LESSION, greeting};
